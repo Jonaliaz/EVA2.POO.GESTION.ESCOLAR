c9ae757e3d88636fee3a68738572ae09cb3d4e25
Lowkyd <leonardobrrg@gmail.com>
2026-09-16 17:25:54 -0300

    listo

--- datos/sql/2026.09.08_14.25_DDL_Escuela.sql
+++ datos/sql/2026.09.08_14.25_DDL_Escuela.sql
@@ -1,3 +1,43 @@
+--Crear tabla de Curso                 -Listo
+Create table Curso (
+    id_curso int primary key auto_increment not null,
+    nombre_curso varchar(30) not null,
+    nivel_curso varchar(20) not null
+);
+
+
+
+--Crear tabla de Apoderado            --Listo
+Create table Apoderado (
+    id_apoderado int primary key auto_increment not null,
+    nombre_apoderado varchar(40) not null,
+    correo_apoderado varchar(50) not null,
+    telefono_apoderado varchar(50) not null
+);
+
+
+
+--Crear tabla de Asignatura              --Listo
+create table Asignatura (
+    id_asignatura int primary key auto_increment not null,
+    nombre_asignatura varchar(40) not null,
+    descripcion_asignatura varchar(100)
+);
+
+
+
+--Crear tabla de Profesor                --Listo
+Create table Profesor (
+    id_profesor int primary key auto_increment not null,
+    nombre_profesor varchar(50) not null,
+    correo_profesor varchar(60) not null,
+    telefono_profesor varchar(18) not null,
+    id_asignatura int not null,
+    foreign key (id_asignatura) references Asignatura(id_asignatura)
+);
+
+
+
 --Crear tabla de Alumno                 --Listo
 Create table Alumno (
     id_alumno int Primary key auto_increment not null,
@@ -25,51 +65,11 @@ Create table Nota (
 
 
 
---Crear tabla de Asignatura              --Listo
-create table Asignatura (
-    id_asignatura int primary key auto_increment not null,
-    nombre_asignatura varchar(40) not null,
-    descripcion_asignatura varchar(100)
-);
-
-
-
---Crear tabla de Profesor                --Listo
-Create table Profesor (
-    id_profesor int primary key auto_increment not null,
-    nombre_profesor varchar(50) not null,
-    correo_profesor varchar(60) not null,
-    telefono_profesor varchar(18) not null,
-    id_asignatura int not null,
-    foreign key(id_asignatura) references Asignatura(id_asignatura)
-);
-
-
-
 --Crear tabla de Asistencia             --Listo
 Create table Asistencia (
     id_asistencia int primary key auto_increment not null,
     fecha_asistencia date not null,
     estado_asistencia varchar(30) not null,
-    id_alumno int not null
-    foreign key(id_alumno) references Alumno(id_alumno)
-);
-
-
-
---Crear tabla de Curso                 -Listo
-Create table Curso (
-    id_curso int primary key auto_increment not null,
-    nombre_curso varchar(30) not null,
-    nivel_curso varchar(20) not null
-);
-
-
-
---Crear tabla de Apoderado            --Listo
-Create table Apoderado (
-    id_apoderado int primary key auto_increment not null,
-    nombre_apoderado varchar(40) not null,
-    correo_apoderado varchar(50) not null,
-    telefono_apoderado, varchar(50) not null,
+    id_alumno int not null,
+    foreign key (id_alumno) references Alumno(id_alumno)
 );
