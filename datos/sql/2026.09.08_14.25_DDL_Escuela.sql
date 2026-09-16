5ca11d73282fc2cb2e1d94e444936d3f60263443
Lowkyd <leonardobrrg@gmail.com>
2026-09-16 15:24:26 -0300

    casi termino de tabla

--- datos/sql/2026.09.08_14.25_DDL_Escuela.sql
+++ datos/sql/2026.09.08_14.25_DDL_Escuela.sql
@@ -5,7 +5,6 @@ create table Periodo_Academico (
     fecha_fin date not null,
     primary key (id_periodo_academico)
 );
-
 --Crear tabla de Alumno
 Create table Alumno (
     id_alumno int not null Primary key,
@@ -18,28 +17,58 @@ Create table Alumno (
 
 
 --Crear tabla de Nota
-
+Create table Nota (
+    id_nota varchar(50) not null primary key,
+    calificacion int not null,
+    fecha date not null,
+    alumno ----(completar despues)
+)
 
 
 
 --Crear tabla de Asignatura
-
+create table Asignatura (
+    id_asignatura int not null primary key,
+    nombre_asignatura varchar(40) not null,
+    descripcion_asignatura varchar(100)
+)
 
 
 
 --Crear tabla de Profesor
-
+Create table Profesor (
+    id_profesor int not null primary key,
+    nombre_profesor varchar(50) not null,
+    correo_profesor varchar(60),
+    telefono_profesor varchar(18),
+    asigunatura_profesor --(Rellenar despues)
+)
 
 
 
 --Crear tabla de Asistencia
-
+Create table Asistencia (
+    id_asistencia int not null primary key,
+    fecha_asistencia date not null,
+    estado_asistencia varchar(30),
+    alumno --(despues rellenar)
+)
 
 
 
 --Crear tabla de Curso
-
+Create table Curso (
+    id_curso int not null primary key,
+    nombre_curso varchar(30) not null,
+    nivel_curso varchar(20)
+)
 
 
 
 --Crear tabla de Apoderado
+Create table Apoderado (
+    id_apoderado int not null primary key,
+    nombre_apoderado varchar(40) not null,
+    correo_apoderado varchar(50) not null,
+    telefono_apoderado, varchar(50) not null
+)
