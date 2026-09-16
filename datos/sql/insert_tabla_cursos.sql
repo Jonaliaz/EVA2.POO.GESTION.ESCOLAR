6fc79648551eb7d1363b9caea255a1e5f4d4425d
Lowkyd <leonardobrrg@gmail.com>
2026-09-16 17:49:36 -0300

    Ingreso de datos

---
+++ datos/sql/insert_tabla_cursos.sql
@@ -0,0 +1,9 @@
+--Ingresar datos a Curso
+insert into Curso 
+(nombre_curso, nivel_curso)
+values 
+('Analista programador','Primer año'),
+('Desarrollador de software', 'Segundo año'),
+('Ingenieria en Informatica','Tercer año');
+
+--Ingresar datos a 
